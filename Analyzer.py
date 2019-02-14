70709953ccb956bb2667929a9ba5f0041bb7b2ab
vansoncc <43052978+vansoncc@users.noreply.github.com>
2019-02-14 17:16:04 -0500

    Add files via upload

--- Analyzer.py
+++ Analyzer.py
@@ -1,6 +1,7 @@
-class Analyzer():
-    
-    def __init__(self, Systolic_BP, Diastolic_BP, Heart_Rate, Heart_O2_Level, Body_temp):
+class Analyzer:
+    def __init__(
+        self, Systolic_BP, Diastolic_BP, Heart_Rate, Heart_O2_Level, Body_temp
+    ):
         self.Systolic_BP = Systolic_BP
         self.Diastolic_BP = Diastolic_BP
         self.Heart_Rate = Heart_Rate
@@ -9,36 +10,37 @@ class Analyzer():
 
     def Signal_Loss(self, Heart_Rate, Body_temp):
         # Signal loss judgement
-        if (Heart_Rate < 60 and Body_temp < 36):
+        if Heart_Rate < 60 and Body_temp < 36:
             return True
         return False
 
     def Shock_Alert(self, Heart_Rate, Body_temp):
         # Shock emergency judgement
-        if (Heart_Rate < 60 and Body_temp >= 36):
+        if Heart_Rate < 60 and Body_temp >= 36:
             return True
         return False
-    
+
     def Oxygen_Supply(self, Heart_O2_Level):
         # Oxygen supply judgement
-        if (Heart_O2_Level < 70):
+        if Heart_O2_Level < 70:
             return True
         return False
-    
+
     def Fever(self, Body_temp):
         # Fever judgement
-        if (Body_temp > 37.5):
+        if Body_temp > 37.5:
             return True
         return False
-    
+
     def Hypotension(self, Systolic_BP, Diastolic_BP):
         # Hypotension judgement
-        if (Systolic_BP < 90 and Diastolic_BP < 60):
+        if Systolic_BP < 90 and Diastolic_BP < 60:
             return True
         return False
-    
+
     def Hypertension(self, Systolic_BP, Diastolic_BP):
         # Hypertension judgement
-        if (Systolic_BP > 140 or Diastolic_BP > 90):
+        if Systolic_BP > 140 or Diastolic_BP > 90:
             return True
         return False
+
